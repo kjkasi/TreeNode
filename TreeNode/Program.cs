f790cdd12067a73720d3bf4e7115fa7ecb9c5627
kjkasi <5842602+kjkasi@users.noreply.github.com>
2023-01-02 13:34:57 +0300

    No comment

---
+++ TreeNode/Program.cs
@@ -0,0 +1,54 @@
+﻿var root = new TreeNode<int>
+{
+    Value = 10,
+    Child = new TreeNode<int>
+    {
+        Value = 20,
+        Child = new TreeNode<int>
+        {
+            Value = 30
+        }
+    }
+};
+
+var stackValues = GetStackValues(root);
+var recurseValues = GetRecurseValues(root);
+
+Console.WriteLine(string.Join(",", stackValues));
+Console.WriteLine(string.Join(",", recurseValues));
+
+IEnumerable<T> GetStackValues<T>(TreeNode<T> root)
+{
+    Stack<TreeNode<T>> stack = new Stack<TreeNode<T>>();
+    stack.Push(root);
+    while (stack.Count > 0)
+    {
+        var node = stack.Pop();
+        if (node == null)
+        {
+            continue;
+        }
+        yield return node.Value;
+        stack.Push(node.Child);
+    }
+}
+
+IEnumerable<T> GetRecurseValues<T>(TreeNode<T> root)
+{
+    if (root == null)
+    {
+        yield break;
+    }
+
+    yield return root.Value;
+    foreach(var item in GetRecurseValues(root.Child))
+    {
+        yield return item;
+    }
+}
+
+public class TreeNode<T>
+{
+    public T Value { get; set; }
+    public TreeNode<T> Child { get; set; }
+}
